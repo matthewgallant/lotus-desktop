5d2eecc3b3213488249a5d4d73733985468fc82b
Matthew Gallant <matt@matthewgallant.me>
2022-05-28 21:03:44 -0400

    Set up react-router-dom

--- src/renderer.js
+++ src/renderer.js
@@ -1,29 +1,16 @@
-/**
- * This file will automatically be loaded by webpack and run in the "renderer" context.
- * To learn more about the differences between the "main" and the "renderer" context in
- * Electron, visit:
- *
- * https://electronjs.org/docs/tutorial/application-architecture#main-and-renderer-processes
- *
- * By default, Node.js integration in this file is disabled. When enabling Node.js integration
- * in a renderer process, please be aware of potential security implications. You can read
- * more about security risks here:
- *
- * https://electronjs.org/docs/tutorial/security
- *
- * To enable Node.js integration in this file, open up `main.js` and enable the `nodeIntegration`
- * flag:
- *
- * ```
- *  // Create the browser window.
- *  mainWindow = new BrowserWindow({
- *    width: 800,
- *    height: 600,
- *    webPreferences: {
- *      nodeIntegration: true
- *    }
- *  });
- * ```
- */
+import * as React from 'react';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/home.jsx'
 
-import './app.jsx';
+const root = ReactDOM.createRoot(
+    document.getElementById("root")
+);
+
+root.render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/main_window" element={<Home />} />
+        </Routes>
+    </BrowserRouter>
+);
