3e26acc7507ddc2eb7cd725e24627b87d0bbf118
Matthew Gallant <matt@matthewgallant.me>
2022-05-31 09:04:58 -0400

    Search field component fixes

--- src/components/searchField.jsx
+++ src/components/searchField.jsx
@@ -1,59 +1,82 @@
 import React from "react"
-import { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 import axios from "axios"
 
 export default function SearchField() {
-    const [searchQuery, setSearchQuery] = useState('')
+    let navigate = useNavigate()
+
+
     const [scryfallSuggestions, setScryfallSuggestions] = useState([])
-    const [activeSuggestion, setActiveSuggestion] = useState(0)
+    const [activeSuggestion, setActiveSuggestion] = useState(-1)
 
     const onChange = (e) => {
-        setSearchQuery(e.target.value)
         requestScryfallSuggestions(e.target.value)
     }
 
     const onKeyDown = (e) => {
         switch (e.keyCode) {
+            
+            // Down arrow
             case 38:
-                setActiveSuggestion(activeSuggestion - 1)
+                if (activeSuggestion > 0) {
+                    setActiveSuggestion(activeSuggestion - 1)
+                }
                 break;
+            
+            // Up arrow
             case 40:
-                setActiveSuggestion(activeSuggestion + 1)
+                if (activeSuggestion < scryfallSuggestions.length - 1) {
+                    setActiveSuggestion(activeSuggestion + 1)
+                }
                 break;
         }
     }
 
-    const onBlur = () => {
+    const onBlur = (e) => {
+        if (!e.currentTarget.contains(e.relatedTarget)) {
         setScryfallSuggestions([])
+        setActiveSuggestion(-1)
+    }
     }
 
     const onSubmit = (e) => {
         e.preventDefault()
-        console.log('Submitting Search...')
+        
+        if (activeSuggestion >= 0) {
+            setScryfallSuggestions([])
+            setActiveSuggestion(-1)
+            navigate(`/card/${scryfallSuggestions[activeSuggestion]}`)
+        }
     }
 
     const requestScryfallSuggestions = (query) => {
-        if (query.length >= 2) {
-            axios.get('https://api.scryfall.com/cards/autocomplete',
-                { params: { q: query }
-            }).then(res => {
+
+        // Only submit autocomplete request if query is at least two characters
+        if (query.length > 1) {
+
+            // Submit autocomplete request
+            axios.get('https://api.scryfall.com/cards/autocomplete', { params: { q: query }}).then(res => {
                 if (res.data.data.length > 0) {
-                    setScryfallSuggestions(res.data.data.slice(0, 4))
+                    setScryfallSuggestions(res.data.data.slice(0, 6))
+                    setActiveSuggestion(-1)
                 } else {
                     setScryfallSuggestions([])
+                    setActiveSuggestion(-1)
                 }
-            })
-            .catch(function () {
+            }).catch(function () {
                 console.log("Could not contact Scryfall's API!")
+                setScryfallSuggestions([])
+                setActiveSuggestion(-1)
             })
         } else {
             setScryfallSuggestions([])
+            setActiveSuggestion(-1)
         }
     }
 
     return (
-        <form autoComplete="off" onSubmit={onSubmit} className="Search__Form">
+        <form autoComplete="off" onSubmit={onSubmit} onBlur={onBlur} className="Search__Form">
             <input
                 type="text"
                 name="search"
@@ -62,12 +85,11 @@ export default function SearchField() {
                 placeholder="Search for any card"
                 spellCheck="false"
                 onChange={onChange}
-                onKeyDown={onKeyDown}
-                onBlur={onBlur} />
+                onKeyDown={onKeyDown} />
             <div className="Search__Suggestions">
                 {scryfallSuggestions.map((suggestion, index) => 
                     <div key={index}>
-                        <Link to="/" className={"Search__Suggestion" + (index == activeSuggestion ? " Search__Active" : '')}>
+                        <Link to="/" className={"Search__Suggestion " + (index == activeSuggestion && "Search__Active")}>
                             {suggestion}
                         </Link>
                     </div>
