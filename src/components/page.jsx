3d9dfddd1d564793996a6c6b76c181255f853b4a
Matthew Gallant <matt@matthewgallant.me>
2022-06-02 13:51:08 -0400

    Add back/forward navigation buttons

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -1,9 +1,13 @@
 import React from "react"
+import { useNavigate } from "react-router-dom"
+
 import Sidebar from "./sidebar.jsx"
 import Spinner from "./spinner.jsx"
 import SearchField from "./searchField.jsx";
 
 export default function Page(props) {
+    const navigate = useNavigate()
+
     document.title = `Lotus | ${props.title}`
 
     return (
@@ -13,8 +17,17 @@ export default function Page(props) {
             </div>
             <div className="Page__Container">
                 <div className="Page__Bar">
-                    <h2 className="Page__Title">{props.title}</h2>
-                    <SearchField />
+                    <div>
+                        <button onClick={() => navigate(-1)} className="bg-transparent border-0 text-light fs-4">
+                            <i className="bi bi-chevron-left"></i>
+                        </button>
+                        <button onClick={() => navigate(1)} className="bg-transparent border-0 text-light fs-4">
+                            <i className="bi bi-chevron-right"></i>
+                        </button>
+                    </div>
+                    <div>
+                        <SearchField />
+                    </div>
                 </div>
                 <div className={"Page__Content " + (!props.noPadding && "Page__Content--Padded")}>
                     {props.isLoading ? <Spinner /> : props.children}
